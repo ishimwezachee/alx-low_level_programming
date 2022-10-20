405cc1b8bd62b28d97a0dc8337a9660adbd6615a
ishimwezachee <zacheeishimwe@gmail.com>
2022-10-20 02:48:47 -0700

    fix error

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
- /**
- * main - tests function that prints if integer is positive or negative
+
+/**
+ * main - Test function for postive or negative
  * Return: 0
  */
+
 
 int main(void)
 {
@@ -13,7 +15,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
-
 
 	return (0);
 }
