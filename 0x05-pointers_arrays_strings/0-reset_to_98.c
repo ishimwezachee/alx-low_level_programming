e9a5311b3dde10c1dfbee55721de24b75b6c6159
ishimwezachee <zacheeishimwe@gmail.com>
2022-10-24 11:20:03 -0700

    fix error

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * owned By zachee
+ * Return: 0 is sccess
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
